34745ae333a5c755b852e224219f921d0a5b83c2
ShaikNeha <126245375+ShaikNeha@users.noreply.github.com>
2023-03-08 16:51:08 +0530

    Add files via upload

---
+++ Testimmutablestring.java
@@ -0,0 +1,15 @@
+class Testimmutablestring
+{
+	public static void main(String[] args) 
+	{
+		String S="Sachin";
+		S=S.concat("Tendulkar");
+	
+		
+		System.out.println(S);
+
+		
+		
+
+	}
+}
---
+++ Teststringconcatenation.java
@@ -0,0 +1,13 @@
+class Teststringconcatenation
+{
+	public static void main(String[] args) 
+	{
+		String s="Sachin"+"Tendulkat";
+		
+		System.out.println(s);
+
+		
+		
+
+	}
+}
