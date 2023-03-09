29a76e0b538c24820cf4f068a74732200c3a2336
ShaikNeha <126245375+ShaikNeha@users.noreply.github.com>
2023-03-09 15:22:23 +0530

    Add files via upload

---
+++ Testsuper4.java
@@ -0,0 +1,21 @@
+class Animal
+{
+	Animal()
+	{
+		System.out.println("Animal is created");
+	}
+}
+class Dog extends Animal{
+	Dog(){
+	System.out.println("Dog is created");
+	}	
+}
+
+class Testsuper4
+{
+	public static void main(String[] args) 
+	{
+		Dog d=new Dog();
+	
+	}
+}
