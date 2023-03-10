fe48e33422bd855871f4b4631bd77c40ed4f482d
ShaikNeha <126245375+ShaikNeha@users.noreply.github.com>
2023-03-10 16:55:50 +0530

    Add files via upload

---
+++ Testanonymous.java
@@ -0,0 +1,16 @@
+class Testanonymous 
+{
+	static void pointArray(int arr[]){
+		
+		for(int i=1;i<arr.length;i++)
+			
+	System.out.println(arr[i]);
+	}
+
+	public static void main(String[] args) 
+	{
+		pointArray(new int[]
+		{10,22,44,66});
+		
+	}
+}
